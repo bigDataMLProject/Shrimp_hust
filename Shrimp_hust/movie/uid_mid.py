781ae6d97531e6e6dc9389df218c0bcf6c5083a3
TCZdudu <1456164189@qq.com>
2018-07-02 09:51:29 +0800

    0702

--- Shrimp_hust/movie/uid_mid.py
+++ Shrimp_hust/movie/uid_mid.py
@@ -1,24 +1,25 @@
 from pyspark.sql import SQLContext
 from pyspark import SparkContext
 from pyspark import SparkConf
+from pyspark.mllib.recommendation import MatrixFactorizationModel
 
 conf = SparkConf().setAppName("miniproject").setMaster("local[*]")
 sc = SparkContext.getOrCreate(conf)
 sqlContext = SQLContext(sc)
 
 def recommendationM(uid):
-    '''
     url_ = "jdbc:mysql://wander:3306/big_data"
     driver_ = "com.mysql.jdbc.Driver"
-    table = "rating"
+    table = "user"
     dataf = sqlContext.read.format("jdbc").options(url=url_, driver=driver_, dbtable=table, user="root", password="qwer123").load()
     sqlContext.registerDataFrameAsTable(dataf,'user')
-    '''
 
-    id =  sqlContext.sql('select id from user where u_id ='+str(uid)).take(1)[0]['id']
+    id =  sqlContext.sql("select id from user where user.u_id ="+"'"+str(uid)+"'").take(1)[0]['id']
 
     model=MatrixFactorizationModel.load(sc,'/tmp/model')
-    result = list(map(lambda x:x[1],model.recommendProducts(id,10)))
+    result = list(map(lambda x:x[1],model.recommendProducts(id,13)))
+    print(result)
 
-if __name__ == '__main__':
-    print(recommendationM('1000545'))
+print(recommendationM('zzxxyy'))
+
+
---
+++ Shrimp_hust/static/detail.js
@@ -0,0 +1,19 @@
+ /**
+ * Created by tcz on 18-7-1.
+ */
+
+$(document).ready(function () {
+    data = sessionStorage.getItem("key");
+    res = JSON.parse(data);
+    //alert(res["name"]);
+    document.getElementById("name").textContent = res["name"];
+    document.getElementById("src").src = res["src"];
+    document.getElementById("director").textContent ="导演：" + res["director"];
+    document.getElementById("editor").textContent ="编剧：" + res["editor"];
+    document.getElementById("actor").textContent = "演员：" + res["actor"];
+    document.getElementById("type").textContent ="类型：" + res["type"];
+    document.getElementById("area").textContent ="国家/地区：" + res["area"];
+    document.getElementById("time").textContent ="时长：" + res["time"] +"分钟";
+    document.getElementById("rate").textContent ="评分：" + res["rate"];
+    }
+)
